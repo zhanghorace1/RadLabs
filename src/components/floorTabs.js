da3f73f32d0403bf883a83279c40ea00fc1695eb
HoraceZ <zhanghorace@gmail.com>
2022-09-07 14:33:49 -0400

    added floor tabs and floor tables

--- src/components/floorTabs.js
+++ src/components/floorTabs.js
@@ -1,9 +1,22 @@
+import { Box, Button } from '@mui/material';
 import  data from '../assets/buildingData.json';
+import { FloorTable } from './floorTable';
 
 export function FloorTabs() {
   return (
-    <div>
-      {data.name}
-    </div>
+    <Box
+    style={{ display: "flex", flexDirection: 'row', padding: 10, margin: 10, justifyContent:"center" }}
+    >
+      {data.floors.map((floor)=> {
+        return (
+          <Box>
+          <Button>
+            {floor.name}
+          </Button>
+          <FloorTable floorData={floor}/>
+          </Box>
+          )
+      })}
+    </Box>
   );
 }
