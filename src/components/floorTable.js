6cf97bf2f0844d4f74c599be943a0345a9967224
HoraceZ <zhanghorace@gmail.com>
2022-09-07 23:35:15 -0400

    setup floor button toggle

--- src/components/floorTable.js
+++ src/components/floorTable.js
@@ -1,10 +1,13 @@
 import { Box, Button } from '@mui/material';
 
 export function FloorTable(floorData) {
-  console.log('FLOORRRR', floorData)
+
+
+  console.log('FLOORRRR', floorData )
   return (
     <Box>
-  <h1>This is {floorData.floorData.name}</h1>
+      <h1>This is {floorData.floorData.name}</h1>
+
     </Box>
   );
 }
